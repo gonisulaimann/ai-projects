94aa623e2185eb36a243b293c94a0d040d4030ed
Goni <goni@centreforenergy.org>
2026-01-17 23:14:19 +0100

    Day1: Learning Python

--- basics.py
+++ basics.py
@@ -12,3 +12,14 @@ def function():
   a = "Goni"
 function()
 print("My name is " + a)
+
+d = 5
+print(type(d))
+e = "Hello"
+print(type(e))
+f = 3.14
+print(type(f))
+g = True
+print(type(g))
+h = {"apple"}
+print(type(h))
