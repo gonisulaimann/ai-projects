c59fad27f71f2b5c0223d7f139df05ec0bc58c86
Goni <goni@centreforenergy.org>
2026-01-17 22:04:25 +0100

    Day1: Python

---
+++ basics.py
@@ -0,0 +1,14 @@
+x = "Excellent"
+def myfunc2():
+  global x
+  x = "superb"
+myfunc2()
+print("Python is " + x)
+
+
+a = "Harun"
+def function():
+  global a
+  a = "Goni"
+function()
+print("My name is " + a)
